9baebc29d0c1c1ffcd2cf45176096aa6f51153c7
minseo <msp05070478@gmail.com>
2021-10-15 00:03:30 +0900

    third commit

--- app/src/main/java/com/example/chat_app/ui/SignUp.kt
+++ app/src/main/java/com/example/chat_app/ui/SignUp.kt
@@ -21,30 +21,37 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        mAuth = FirebaseAuth.getInstance()
+
         edtEmail = findViewById(R.id.et_email)
         edtPassword = findViewById(R.id.et_password)
         edtname = findViewById(R.id.et_name)
         btnSignUp = findViewById(R.id.btn_Signup)
 
         btnSignUp.setOnClickListener {
+            val name = edtname.text.toString()
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
 
-            signUp(email, password)
+            signUp(name, email, password)
         }
     }
 
-    private fun signUp(email:String, password:String){
+    private fun signUp(name:String, email:String, password:String){
         //유저 회원가입
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     //메인으로 이동
+                    addUserToDatabase(name, email, mAuth.currentUser?.uid!!)
                     val intent = Intent(this@SignUp, MainActivity::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this@SignUp, "이메일 또는 비밀번호가 틀렸습니다", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+    private fun addUserToDatabase(name: String, email: String, uid: String){
+
     }
 }
