d2a723f7ee187282e9c0fa6798dc482bdd8bdb2e
minseo <msp05070478@gmail.com>
2021-10-14 23:02:56 +0900

    second commit

--- app/src/main/java/com/example/chat_app/ui/Login.kt
+++ app/src/main/java/com/example/chat_app/ui/Login.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.chat_app.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -13,10 +15,13 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword : EditText
     private lateinit var btnLogin : Button
     private lateinit var btnSignUp : Button
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.et_email)
         edtPassword = findViewById(R.id.et_password)
@@ -27,5 +32,25 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email:String, password:String){
+        //유저 로그인 정보 확인
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //유저 정보 확인 시 로그인
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "유저 정보가 존재하지 않습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/chat_app/ui/SignUp.kt
+++ app/src/main/java/com/example/chat_app/ui/SignUp.kt
@@ -1,12 +1,50 @@
 package com.example.chat_app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.chat_app.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtname : EditText
+    private lateinit var edtEmail : EditText
+    private lateinit var edtPassword : EditText
+    private lateinit var btnLogin : Button
+    private lateinit var btnSignUp : Button
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        edtEmail = findViewById(R.id.et_email)
+        edtPassword = findViewById(R.id.et_password)
+        edtname = findViewById(R.id.et_name)
+        btnSignUp = findViewById(R.id.btn_Signup)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
+        }
+    }
+
+    private fun signUp(email:String, password:String){
+        //유저 회원가입
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //메인으로 이동
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@SignUp, "이메일 또는 비밀번호가 틀렸습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
